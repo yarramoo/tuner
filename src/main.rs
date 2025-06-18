6fedaf01120479a814a97d37b28fd00a852bce56
Mitchell Moore <mitchell.moore1666@gmail.com>
2025-06-18 19:18:14 +1000

    Added timer to sample at regular intervals

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,22 @@ use microbit::{
     board::Board,
     display::blocking::Display,
     hal::{
-        gpio::{Level, OpenDrainConfig},
-        saadc::SaadcConfig,
-        Saadc, Timer,
-    }, pac::saadc::SAMPLERATE,
+        gpio::{Level, OpenDrainConfig}, saadc::SaadcConfig, timer::{OneShot, Periodic}, Saadc, Timer
+    }, pac::{saadc::SAMPLERATE, TIMER0},
 };
 
 const SAMPLE_RATE: u64 = 8_000;
 const SAMPLE_DELAY: u64 = 1_000_000 / SAMPLE_RATE;
+const SAMPLE_PERIOD_US: u32 = 125;
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
     let mut board = Board::take().unwrap();
-    let mut timer = Timer::new(board.TIMER0);
-    timer.into_periodic();
+    let timer = Timer::new(board.TIMER0);
+    let mut timer = timer.into_periodic();
     
-
     let mut display = Display::new(board.display_pins);
 
     // initialize adc
@@ -45,9 +43,11 @@ fn main() -> ! {
     let mut sum = 0;
     let (mut max, mut min) = (0, 0);
 
-
+    timer.start(SAMPLE_PERIOD_US);
 
     loop {
+        timer_wait(&mut timer);
+        // rprintln!("cycles={}", cycles);
         let mic_value = saadc.read_channel(&mut mic_in).expect("could not read value of microphone") as u16;
         count += 1;
         if count % 10000 == 0 {
@@ -57,6 +57,19 @@ fn main() -> ! {
         sum += mic_value as u64;
         max = max.max(mic_value as u64);
         min = min.min(mic_value as u64);
-        rprintln!("{}, avg={}, max={}, min={}", mic_value, sum / count, max, min);
+        // rprintln!("{}, avg={}, max={}, min={}", mic_value, sum / count, max, min);
+        
+    }
+}
+
+fn timer_wait(timer: &mut Timer<TIMER0, Periodic>) {
+    loop {
+        match timer.reset_if_finished() {
+            true => {
+                return;
+            },
+            false => {},
+        }
+        // rprintln!("stuck :(");
     }
 }
